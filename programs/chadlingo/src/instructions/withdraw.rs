1a34ed19ce0a1babe0a37da38d8dfc7cb34a8e6f
Nico <nicolasvergne88@gmail.com>
2023-10-29 23:30:18 +0000

    refactor: cut down one big file into multiple files

---
+++ programs/chadlingo/src/instructions/withdraw.rs
@@ -0,0 +1,31 @@
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+
+use crate::states::*;
+
+pub fn withdraw_(ctx: Context<Withdraw>) -> ProgramResult {
+    let owner = &mut ctx.accounts.owner;
+    let vault = &mut ctx.accounts.vault;
+
+    if vault.owner != owner.key() {
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    if vault.counter < 30 {
+        return Err(ProgramError::Custom(1));
+    }
+
+    **vault.to_account_info().try_borrow_mut_lamports()? -= vault.balance;
+    **owner.to_account_info().try_borrow_mut_lamports()? += vault.balance;
+    Ok(())
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut, close = owner)]
+    pub vault: Account<'info, Vault>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
