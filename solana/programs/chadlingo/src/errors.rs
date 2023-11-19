8558524f6cb190950e8fa66d436b386ba629de14
Nico <nicolasvergne88@gmail.com>
2023-11-19 21:29:15 +0000

    add new errors

--- solana/programs/chadlingo/src/errors.rs
+++ solana/programs/chadlingo/src/errors.rs
@@ -8,14 +8,20 @@ pub enum CreateError {
 
 #[error_code]
 pub enum DepositError {
-    #[msg("")]
-    ErrorDeposit,
+    #[msg("Deposit needs to be > 0 SOL")]
+    AmountTooLow,
+    #[msg("Deposit needs to be <= 5 SOL")]
+    AmountTooHigh,
+    #[msg("Insufficient funds to deposit")]
+    InsufficientFunds
 }
 
 #[error_code]
 pub enum ValidateError {
-    #[msg("")]
-    ErrorValidate,
+    #[msg("Incorrect owner")]
+    IncorrectOwner,
+    #[msg("Validation out of range")]
+    InvalidTime,
 }
 
 #[error_code]
